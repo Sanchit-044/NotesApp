e2b0c9ad68f407fddd45372c7605657304a9e3f2
Sanchit-044 <sanchitbhardwaj044@gmail.com>
2025-08-18 22:41:34 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "https://my-notesapp-1.onrender.com";
 
 export default function Login({ onNavigate, onLogin }) {
   const [email, setEmail] = useState("");
