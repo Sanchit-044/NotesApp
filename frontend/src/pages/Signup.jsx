38cf774975dea74220446ae93bfa58b5ca508207
Sanchit-044 <sanchitbhardwaj044@gmail.com>
2025-08-18 22:41:49 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
+const API = import.meta.env.VITE_API_URL || "https://my-notesapp-1.onrender.com";
 export default function Signup({ onNavigate, onSignup }) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
